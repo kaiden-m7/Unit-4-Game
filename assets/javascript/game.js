5e84a43cc8ab038c630abe128705c323d5fb971f
Kaiden Munsey <kaidenmnsey@aol.com>
2020-04-25 15:38:18 -0400

    <adding onclick listeners to gems>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,26 +6,18 @@ $(document).ready(function() {
     $('#scoreTotal').text(score)
 
     // allows computer to pick randmon number from 19-120
-    let randmonNumber = Math.floor(Math.random() * (120-19)) + 19;
-    $('#cNumber').text(randmonNumber);
-    console.log(randmonNumber);
+    let randomNumber = Math.floor(Math.random() * (120-19)) + 19;
+    $('#cNumber').text(randomNumber);
+    console.log(randomNumber);
 
-    //array for each crtal buttom values at random from 1-12
+    //array for each crystal button values at random from 1-12
     let crystalValues = {};
     crystalValues[1] = Math.floor(Math.random()*12+1);
     crystalValues[2] = Math.floor(Math.random()*12+1);
     crystalValues[3] = Math.floor(Math.random()*12+1);
     crystalValues[4] = Math.floor(Math.random()*12+1);
     console.log(crystalValues);
-    $('#scoreTotal').html(crystalValues);
 
-    //reset after a win or loss 
-    function reset () {
-        randmonNumber = Math.floor(Math.random() * (120-19)) + 19;
-        $('#cNumber').text(randmonNumber);
-        score = 0;
-        crystalValues = [];
-    };
 
     //function for getting the computer number
     function win () {
@@ -41,30 +33,65 @@ $(document).ready(function() {
         reset();
     };
 
-    //generating a total from values of gems
-    let gemNumber = function(min,max,cnt) {
-        let arr=[]
-        for(let i = min,j=0;i<= max; j++,i++)
-        arr[j]=i
-        arr.sort(function() {
-            return Math.floor((MAth.randmon()*3)-1)
-        });
-        return arr.splice(0,cnt)
-    };
 
-    //allowing each crystal click to have a value and add to computer number for win or loss
-    $('.crystal-btn').on("click", function() {
-        let crystalValue = parseInt($(this).val());
-        score += crystalValue;
-        console.log(score);
+    //eventlisteners for each gem to regester their value, for some reason i cannot get them to show in the scoreTotal
+    //i had it coded before it was say 'NAN' in the console log and now i cant get it to show anything
+    $('#btn1').on('click', function (){
+        score = score + crystalValues[1];
+        $('#scoreTotal').text(score);
+            if (score === randomNumber) {
+                win();
+            } else if (score > randomNumber) {
+                loss()
+            }
+        console.log(score)
+    })
+
+    $('#btn2').on('click', function (){
+        score = score + crystalValues[2];
+        $('#scoreTotal').text(score);
+            if (score === randomNumber) {
+                win();
+            } else if (score > randomNumber) {
+                loss()
+            }
+        console.log(score)
+    })
+
+    $('#btn3').on('click', function (){
+        score = score + crystalValues[3];
+        $('#scoreTotal').text(score);
+            if (score === randomNumber) {
+                win();
+            } else if (score > randomNumber) {
+                loss()
+            }
+        console.log(score)
+    })
+
+    $('#btn4').on('click', function (){
+        score = score + crystalValues[4];
+        $('#scoreTotal').text(score);
+            if (score === randomNumber) {
+                win();
+            } else if (score > randomNumber) {
+                loss()
+            }
+        console.log(score)
+    })
+    
+
+    let reset = function () {
+        randomNumber = Math.floor(Math.random() * (120-19)) + 19;
+        console.log(randomNumber);
+        $("#cNumber").text(randomNumber);
+        score = 0
         $("#scoreTotal").html(score);
-        if (score === randmonNumber) {
-            win();
-        }
-        else if (score > randmonNumber) {
-            loss();
-        };
-    });
+        crystalValues[1] = Math.floor(Math.random()*12+1);
+        crystalValues[2] = Math.floor(Math.random()*12+1);
+        crystalValues[3] = Math.floor(Math.random()*12+1);
+        crystalValues[4] = Math.floor(Math.random()*12+1);
+    };
 
 
 
