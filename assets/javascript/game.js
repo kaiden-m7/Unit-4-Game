7b0cb4cc21eb8263b0025da0c33bcb0f9cf821e0
Kaiden Munsey <kaidenmnsey@aol.com>
2020-04-25 09:19:41 -0400

    <adding pseudo code and adding reset wins and loss functions>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,58 @@
 $(document).ready(function (){
+    //global variables
     let wins = 0;
     let losses = 0;
     let score =0;
+    $('#scoreTotal').text(score)
 
+    // allows computer to pick randmon number from 19-120
     let randmonNumber = Math.floor(Math.random() * (120-19)) + 19;
-    console.log(randmonNumber);
     $('#cNumber').text(randmonNumber);
+    console.log(randmonNumber);
 
+    //array for each crtal buttom values at random from 1-12
     let crystalValues = {};
     crystalValues[1] = Math.floor(Math.random()*12+1);
     crystalValues[2] = Math.floor(Math.random()*12+1);
     crystalValues[3] = Math.floor(Math.random()*12+1);
     crystalValues[4] = Math.floor(Math.random()*12+1);
+    console.log(crystalValues);
 
-    
+    //reset after a win or loss 
+    function reset () {
+        randmonNumber = Math.floor(Math.random() * (120-19)) + 19;
+        $('#cNumber').text(randmonNumber);
+        score = 0;
+        crystalValues = [];
+    };
 
+    //functio for getting the computer number
+    function wins () {
+        wins++;
+        $("#wins").html("Wins: " + wins);
+        reset();
+    };
+
+    //function for not getting the computer number 
+    function losses () {
+        losses++;
+        $("#losses").html("Losses: " + losses);
+        reset();
+    };
+
+    //allowing each crystal click to have a value and add to computer number for win or loss
+    $('.crystal-btn').on("click", function(){
+        let crystalValue = parseInt($(this).val());
+        score += crystalValue;
+        console.log(score);
+        $("#scoreTotal").html(score);
+        if (score === randmonNumber) {
+            wins();
+        }
+        else if (score > randmonNumber) {
+            losses();
+        };
+    };
 
 
 
